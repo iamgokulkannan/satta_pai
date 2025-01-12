91635796705d27d9396dc88201d0e8f752a3a86c
gokul <ggokulkannan2006@gmail.com>
2025-01-12 14:20:00 +0530

    updated scrolling and background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Intro from './components/intro';
 import NavBar from './components/navBar';
 import InfinteScroll from './components/infinteScroll';
 import InfinteScrollReverse from './components/infinteScrollReverse';
+import './App.css';
 function App() {
   return (
     <div className="App">
